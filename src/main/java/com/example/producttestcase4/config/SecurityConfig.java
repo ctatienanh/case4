a11a6666b6c10608d822494edc6fc78f4a0dc715
haireal007 <you@example.com>
2022-08-05 13:52:56 +0700

    14h 5/8

--- src/main/java/com/example/producttestcase4/config/SecurityConfig.java
+++ src/main/java/com/example/producttestcase4/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().ignoringAntMatchers("/");
         http.authorizeRequests()
-                .antMatchers("/single/**","/product/**","/category", "/login/**", "/register/**","/comment/**").permitAll()
+                .antMatchers("/single/**","/product/**","/category/**", "/login/**", "/register/**","/comment/**","/**").permitAll()
 //                .and().authorizeRequests().antMatchers("/user/**").hasRole("USER")
 //                .and().authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
